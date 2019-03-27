eaeba6a906da1b7580f91f7aa257a645c87e2ae5
375956133 <375956133@qq.com>
2019-03-27 16:21:02 +0800

    add2

---
+++ js/collSource-info.js
@@ -0,0 +1,9 @@
+/*
+* @Author: 375956133
+* @Date:   2019-03-27 14:29:39
+* @Last Modified by:   375956133
+* @Last Modified time: 2019-03-27 14:29:59
+*/
+$(function () {
+	 $("#ink_showImg .small_pic .img").imagezoom();
+ });
